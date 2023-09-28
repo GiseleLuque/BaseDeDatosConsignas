4e1fe3f258eeb76325e91825e04b25a51fc04cba
GiseleLuque <lucaluriaud@gmail.com>
2023-09-28 11:45:46 -0300

    traducimos el user y post

---
+++ mod.pgAdmin4/ejercicio2.sql
@@ -0,0 +1,6 @@
+INSERT INTO "user" (username,password)
+  VALUES ('Iara', 'Arbol'),('Evangelin', 'nubecita'),('tobias','casa');
+
+
+INSERT INTO "post" (author_id,title,body)
+  VALUES (1, 'Caty', 'Manzana'), (2, 'Dilan', 'dilan'),(3,'Romeo','lkjhgfdsa');
